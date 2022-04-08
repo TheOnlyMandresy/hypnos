8749a3229e4616ef4290c09a0d80b21dcec8eba4
Randriatavy Mandresy <mandresyrvy@gmail.com>
2022-04-08 18:42:02 +0200

    Navbar improvement

--- src/Views/Templates/Header.php
+++ src/Views/Templates/Header.php
@@ -1,5 +1,43 @@
 <nav id="navbar">
     <img src="img/hypnos.png" alt="company name" />
+
+    <div class="nav">
+        <ul>
+            <li>
+                <a><p class="btn-nav">
+                    <span>Nos bâtiments</span>
+                </p></a>
+            </li>
+            <li>
+                <a><p class="btn-nav">
+                    <span>Nos chambres</span>
+                </p></a>
+            </li>
+            <li>
+                <a><p class="btn-nav">
+                    <span>Faire une réservation</span>
+                </p></a>
+            </li>
+        </ul>
+
+        <ul>
+            <li>
+                <a><p class="btn-nav">
+                    <span>Se connecter</span>
+                </p></a>
+            </li>
+            <li>
+                <a><p class="btn-nav">
+                    <span>S'inscrire</span>
+                </p></a>
+            </li>
+            <li>
+                <a><p class="btn-nav">
+                    <span>Nous contacter</span>
+                </p></a>
+            </li>
+        </ul>
+    </div>
 </nav>
 
 <div class="logo">
